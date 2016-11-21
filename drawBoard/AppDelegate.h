8181191645df0e7474f89d859c5b8e0ecb86d270
Capital <303624150@qq.com>
2016-11-21 17:38:23 +0800

    Initial Commit

---
+++ drawBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drawBoard
+//
+//  Created by rwli on 16/11/21.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
